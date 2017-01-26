53505cc0780236787c5791a9c08cf4703aeacb3d
Давиденко Дмитрий Вячеславович <dmitry.davidenko@eltex.loc>
2017-01-26 17:08:32 +0600

    Config format switched to JSON

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -4,20 +4,22 @@ import sys
 import os
 import requests
 from urlparse import urlparse
+import json
 
-if os.path.isfile("/etc/requests.txt"):
-	file = "/etc/requests.txt"
+if os.path.isfile("./requests.json"):
+	config = "./requests.json"
 else:
-	if os.path.isfile("~/requests.txt"):
-		file = "~/requests.txt"
+	if os.path.isfile("~/requests.json"):
+		config = "~/requests.txt"
 	else:
-		if os.path.isfile("./requests.txt"):
-			file = "./requests.txt"
+		if os.path.isfile("/etc/requests.json"):
+			config = "/etc/requests.json"
 		else:
-			raise "no requests.txt specified"
+			raise Exception("no requests.json provided")
 
-with open(file) as f:
-    request_list = f.readlines()
+with open(config) as f:
+    request_list = json.load(f)
+print(request_list)
 
 #TODO: Дать возможность конфигурировать User-Agent
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'}
@@ -25,11 +27,15 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko
 f = codecs.open('index_new.html', mode='w', encoding='utf-8')
 
 for req in request_list:
-	result = requests.get(req, headers = headers)
-	parsed_uri = urlparse(req)
+	result = requests.get(req["url"], headers = headers)
+	parsed_uri = urlparse(req["url"])
 	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-	page1 = re.sub("src=\"", "src=\"" + domain, result.text)
-	page2 = re.sub("href=\"", "href=\"" + domain, result.text)
+	if "title" in req:
+		title = req["title"]
+	else:
+		title = ""
+	page1 = title + re.sub("src=\"", "src=\"" + domain, result.text)
+	page2 = re.sub("href=\"", "href=\"" + domain, page1)
 	f.write(page2)
 
 f.close()
