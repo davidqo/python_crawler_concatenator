40f6df7680399e5ff696546eb0c8c8c6deddf8bf
Давиденко Дмитрий Вячеславович <dmitry.davidenko@eltex.loc>
2017-01-27 10:50:58 +0600

    We can now ignore specified substrings using file 'ignore'

--- reload.sh
+++ reload.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Функция подгатавливает файлы для сравнения удаляя из обоих файлов
+# подстроки соответствующие паттернам, указанным в файле ignore
+function prepare_to_diff() {
+        FILENAME=$1
+        FILE_CONTENTS=$(<$FILENAME)
+
+        while read -r line
+        do
+                #Экранируем в паттерне / так как он используется
+                #как разделитель в sed
+                ESCAPED1=$(sed -e "s/\//\\\\\//g" <<< $line)
+                printf -v ESCAPED2 "%q" "$ESCAPED1"
+                FILE_CONTENTS=$(echo "$FILE_CONTENTS" | sed "s/$ESCAPED2//g")
+        done < ignore
+
+        echo "$FILE_CONTENTS"
+}
+
 # Если файл index_new.html не существует значит он почему то не был
 # сгенерирован, что очень плохо. Скрипт упал.
 if [ ! -f "index_new.html" ]; then
@@ -16,6 +34,8 @@ else
 	# Проверяем изменился ли файл.
 	# Если файл изменился то перезагружаем его в браузере.
 	# Плагин firefox remote-control должен быть установлен
+        
+	diff <(prepare_to_diff "index.html") <(prepare_to_diff "index_new.html")
 	RESULT=`diff index.html index_new.html`
 	if [ ! -z "$RESULT" ]; then 
 		cp index_new.html index.html
